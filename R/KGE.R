f1388bad36a6a8c3c69dc590ca63686bf91b81ee
Mauricio Zambrano-Bigiarini <hzambran@users.noreply.github.com>
2012-10-18 23:12:06 +0000

    KGE.R: version 2012 implemented

--- R/KGE.R
+++ R/KGE.R
@@ -26,7 +26,7 @@
 # Ref2:
 # Kling, H., M. Fuchs, and M. Paulin (2012), Runoff conditions in the upper
 # Danube basin under an ensemble of climate change scenarios, 
-# Journal of Hydrology, 424-425, 264–277, DOI:10.1016/j.jhydrol.2012.01.011.
+# Journal of Hydrology, 424-425, 264-277, DOI:10.1016/j.jhydrol.2012.01.011.
 
 
 # 'obs' : numeric 'data.frame', 'matrix' or 'vector' with observed values
@@ -37,75 +37,91 @@
 
 KGE <- function(sim, obs, ...) UseMethod("KGE")
 
-KGE.default <- function(sim, obs, s=c(1,1,1), method=c("2009", "2012"), na.rm=TRUE, ...) { 
+KGE.default <- function(sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                        method=c("2009", "2012"), out.type=c("single", "full"), ...) { 
 
-     # If the user provided a value for 's'
-     if (!all.equal(s, c(1,1,1)) )  {
-       if ( length(s) != 3 ) stop("Invalid argument: lenght(s) must be equal to 3 !")
-       if ( sum(s) != 1 )    stop("Invalid argument: sum(s) must be equal to 1.0 !")
-     } # IF end
+  # If the user provided a value for 's'
+  if (!all.equal(s, c(1,1,1)) )  {
+     if ( length(s) != 3 ) stop("Invalid argument: lenght(s) must be equal to 3 !")
+     if ( sum(s) != 1 )    stop("Invalid argument: sum(s) must be equal to 1.0 !")
+  } # IF end
      
-     method <- match.arg(method) 
+  method   <- match.arg(method)
+  out.type <- match.arg(out.type)  
 
-     if ( is.na(match(class(sim), c("integer", "numeric", "ts", "zoo"))) |
-          is.na(match(class(obs), c("integer", "numeric", "ts", "zoo")))
-     ) stop("Invalid argument type: 'sim' & 'obs' have to be of class: c('integer', 'numeric', 'ts', 'zoo')")      
+  if ( is.na(match(class(sim), c("integer", "numeric", "ts", "zoo"))) |
+       is.na(match(class(obs), c("integer", "numeric", "ts", "zoo")))
+  ) stop("Invalid argument type: 'sim' & 'obs' have to be of class: c('integer', 'numeric', 'ts', 'zoo')")      
    
-     vi <- valindex(sim, obs)
+  vi <- valindex(sim, obs)
      
-     if (length(vi) > 0) {
+  if (length(vi) > 0) {
 	 
-       obs <- obs[vi]
-       sim <- sim[vi]
+    obs <- obs[vi]
+    sim <- sim[vi]
 
-       # Mean values
-       mean.sim <- mean(sim, na.rm=na.rm)
-       mean.obs <- mean(obs, na.rm=na.rm)
+    # Mean values
+    mean.sim <- mean(sim, na.rm=na.rm)
+    mean.obs <- mean(obs, na.rm=na.rm)
 
-       # Standard deviations
-       sigma.sim <- sd(sim, na.rm=na.rm)
-       sigma.obs <- sd(obs, na.rm=na.rm)
+    # Standard deviations
+    sigma.sim <- sd(sim, na.rm=na.rm)
+    sigma.obs <- sd(obs, na.rm=na.rm)
          
-       # Pearson product-moment correlation coefficient
-       r     <- .rPearson(sim, obs)
+    # Pearson product-moment correlation coefficient
+    r     <- .rPearson(sim, obs)
 
-       # Alpha is a measure of relative variability between simulated and observed values (See Ref1)
-       Alpha <- sigma.sim / sigma.obs
+    # Alpha is a measure of relative variability between simulated and observed values (See Ref1)
+    Alpha <- sigma.sim / sigma.obs
 
-       # Beta is the ratio between the mean of the simulated values and the mean observed ones
-       Beta <- mean.sim / mean.obs
+    # Beta is the ratio between the mean of the simulated values to the mean of observations
+    Beta <- mean.sim / mean.obs
        
-       # CV.sim is the coefficient of variation of the simulated values [dimensionless]
-       # CV.obs is the coefficient of variation of the observations [dimensionless]
-       CV.sim <- sigma.sim / mean.sim
-       CV.obs <- sigma.obs / mean.obs
+    # CV.sim is the coefficient of variation of the simulated values [dimensionless]
+    # CV.obs is the coefficient of variation of the observations [dimensionless]
+    CV.sim <- sigma.sim / mean.sim
+    CV.obs <- sigma.obs / mean.obs
        
-       # Gamma is the variability ratio, which is used instead of Alpha (See Ref2)
-       Gamma <- CV.sim / CV.obs
+    # Gamma is the variability ratio, which is used instead of Alpha (See Ref2)
+    Gamma <- CV.sim / CV.obs
        
-       # Variability ratio deoending on 'method'
-       ifelse(method=="2012", vr <- Gamma, vr <- Alpha)
+    # Variability ratio depending on 'method'
+    if(method=="2012") {
+      vr     <- Gamma
+      vr.stg <- "Gamma"
+    } else {
+        vr     <- Alpha
+        vr.stg <- "Alpha"
+      } # ELSE end
 
-       # Computation of KGE
-       if ( (mean.obs != 0) | (sigma.obs != 0) ) {
-           KGE <- 1 - sqrt( (s[1]*(r-1))^2 + (s[2]*(vr-1))^2 + (s[3]*(Beta-1))^2 )
-       } else {
-           if ( mean.obs != 0)  warning("Warning: 'mean(obs)==0'. Beta = -Inf")
-           if ( sigma.obs != 0) warning("Warning: 'sd(obs)==0'. Alpha = -Inf")
-           KGE <- NA
-         } # ELSE end  
+    # KGE Computation
+    if ( (mean.obs != 0) | (sigma.obs != 0) ) {
+        KGE <- 1 - sqrt( (s[1]*(r-1))^2 + (s[2]*(vr-1))^2 + (s[3]*(Beta-1))^2 )
+    } else {
+        if ( mean.obs != 0)  warning("Warning: 'mean(obs)==0'. Beta = Inf")
+        if ( sigma.obs != 0) warning("Warning: 'sd(obs)==0'. ", vr.stg, " = Inf")
+        KGE <- NA
+      } # ELSE end  
             
-     } else {
-         KGE <- NA
-         warning("There are no pairs of 'sim' and 'obs' without missing values !")
-       } # ELSE end
+  } else {
+      KGE <- NA
+      warning("There are no pairs of 'sim' and 'obs' without missing values !")
+    } # ELSE end
+
+  if (out.type=="single") {
+        out <- KGE
+  } else {
+      out <- list(KGE.value=KGE, KGE.elements=c(r, Beta, vr))
+      names(out[[2]]) <- c("r", "Beta", vr.stg)
+    } # ELSE end    
  
-     return(KGE)
+  return(out)
      
 } # 'KGE.default' end
 
 
-KGE.matrix <- function (sim, obs, s=c(1,1,1), method=c("2009", "2012"), na.rm=TRUE, ...){ 
+KGE.matrix <- function (sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                        method=c("2009", "2012"), out.type=c("single", "full"), ...){ 
 
   # Checking that 'sim' and 'obs' have the same dimensions
   if ( all.equal(dim(sim), dim(obs)) != TRUE )
@@ -113,27 +129,45 @@ KGE.matrix <- function (sim, obs, s=c(1,1,1), method=c("2009", "2012"), na.rm=TR
            paste(dim(sim), collapse=" "), "] != [", 
            paste(dim(obs), collapse=" "), "] )", sep="") )
            
-  method <- match.arg(method)
+  method   <- match.arg(method)
+  out.type <- match.arg(out.type) 
 
-  KGE <- rep(NA, ncol(obs))       
+  ifelse(method=="2012", vr.stg <- "Gamma", vr.stg <- "Alpha")
+
+  KGE                <- rep(NA, ncol(obs))       
+  elements           <- matrix(NA, nrow=3, ncol=ncol(obs))
+  rownames(elements) <- c("r", "Beta", vr.stg)
+  colnames(elements) <- colnames(obs)
           
-  KGE <- sapply(1:ncol(obs), function(i,x,y) { 
-                 KGE[i] <- KGE.default( x[,i], y[,i], s=s, method=method, na.rm=na.rm, ... )
-               }, x=sim, y=obs )    
-                     
-  names(KGE) <- colnames(obs)
-  return(KGE)
+  if (out.type=="single") {
+    out <- sapply(1:ncol(obs), function(i,x,y) { 
+                   KGE[i] <- KGE.default( x[,i], y[,i], s=s, na.rm=na.rm, method=method, out.type=out.type, ... )
+                 }, x=sim, y=obs )  
+    names(out) <- colnames(obs) 
+  } else { out <- lapply(1:ncol(obs), function(i,x,y) { 
+                         KGE.default( x[,i], y[,i], s=s, na.rm=na.rm, method=method, out.type=out.type, ... )
+                       }, x=sim, y=obs ) 
+            for (i in 1:length(out) ) {
+               KGE[i] <- out[[i]][[1]]
+               elements[,i] <- as.numeric(out[[i]][[2]])
+            } # FOR end 
+            out <- list(KGE.value=KGE, KGE.elements=elements)
+          } # ELSE end                     
+  
+  return(out)
      
 } # 'KGE.matrix' end
 
 
-KGE.data.frame <- function (sim, obs, s=c(1,1,1), method=c("2009", "2012"), na.rm=TRUE, ...){ 
+KGE.data.frame <- function (sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                            method=c("2009", "2012"), out.type=c("single", "full"), ...){ 
  
   sim <- as.matrix(sim)
   obs <- as.matrix(obs)
   
-  method <- match.arg(method)
+  method   <- match.arg(method)
+  out.type <- match.arg(out.type) 
    
-  KGE.matrix(sim, obs, s=s, method=method, na.rm=na.rm, ...)
+  KGE.matrix(sim, obs, s=s, na.rm=na.rm, method=method, out.type=out.type, ...)
      
 } # 'KGE.data.frame' end
