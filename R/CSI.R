64b9baf1dfbeec3844cf458be65c3b1f6a9d49b0
hzambran <mzb.devel@gmail.com>
2016-03-09 20:16:49 -0300

    critical success index (CSI) added

---
+++ R/CSI.R
@@ -0,0 +1,174 @@
+# File CSI.R
+# Part of the hydroGOF package, https://github.com/hzambran/hydroGOF
+# Copyright 2008-2016 Mauricio Zambrano-Bigiarini
+# Distributed under GPL 2 or later
+
+################################################################################
+##                       'CSI': Critical Success Index                        ## 
+##                        (also known as Threat Score)                        ## 
+##                   (also known as Ratio of Verification)                    ## 
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                        ##
+################################################################################
+# Created: 09-Mar-2016                                                        ##
+# Updates:                                                                    ##
+################################################################################
+# Reference: -) Jolliffe, I.T., Stephenson, D.B. (Eds.), 2003.                ##
+#               Forecast verification: A practitioners guide in atmospheric   ##
+#               science. John Wiley & Sons Ltd, England.                      ##
+#            -) http://www.cawcr.gov.au/projects/verification/                ##
+#            -) http://www.wxonline.info/topics/verif2.html                   ##
+################################################################################
+
+# It can be considered as a sample estimate of the conditional probability of a 
+# hit given that the even of interest was either in the SRFE, or in the raingauge, or both.
+# The Critical Success Index (CSI) takes into account both missed events and 
+# false alarms. 
+# CSI ranges from 0 to 1:
+# A CSI of 1 (CSI = 1) indicates a perfect match
+# A CSI of 0 (CSI = 0) indicates that all the events were corectly identified by the simulations.
+# The CSI is somewhat sensitive to the climatology of the event, tending to give poorer scores for rare events
+
+# 'obs'   : 'factor' with observed values. If 'obs' is not a factor, use 'breaks' (when provided) to classify 'obs' in categorical classes (i.e., factor)
+# 'sim'   : 'factor' with simulated values. If 'sim' is not a factor, use 'breaks' (when provided) to classify 'sim' in categorical classes (i.e., factor)
+# 'trgt'  : class/event to be looked for in 'sim' and 'obs' to evaluate the correct events/forecasts
+# 'Result': CSI between 'sim' and 'obs'
+
+CSI <-function(sim, obs, trgt=NA, ...) UseMethod("CSI")
+
+CSI.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, labels=NA, na.rm=TRUE, verbose=TRUE, ...){ 
+
+   if ( is.na(match(class(sim), c("factor"))) | 
+        is.na(match(class(obs), c("factor"))) 
+      ) { if ( (missing(breaks)) ) {
+         stop("Missing argument: 'breaks' have to be provided when 'sim' & 'obs' are not factors !")  
+       } else {
+          if (verbose) message("Using 'breaks' to identify factor classes....")
+          if (!is.na(labels)) {
+            sim <- cut(sim, breaks=breaks, include.lowest=include.lowest, labels=labels)
+            obs <- cut(obs, breaks=breaks, include.lowest=include.lowest, labels=labels)
+          } else {
+              sim <- cut(sim, breaks=breaks, include.lowest=include.lowest)
+              obs <- cut(obs, breaks=breaks, include.lowest=include.lowest)
+            }
+         } 
+       }
+
+   if (!(all.equal(levels(sim), levels(obs))))
+     warning("'sim' and 'obs' do not have the same classes.")   
+
+   if (missing(trgt)) {
+     stop("Missing argument: You have to provide 'trgt' to compute CSI !")    
+   } else {
+     if ( !(trgt %in% levels(sim) ) | !(trgt %in% levels(obs) ) )
+       stop("Invalid argument: 'trgt' has to be present in both 'sim' and 'obs' !")  
+     } # ELSE end
+
+   vi <- valindex(sim, obs)
+     
+   obs <- obs[vi]
+   sim <- sim[vi]
+
+   # Ne: Total number of events
+   Ne <- length(obs)
+     
+   # H: Hits
+   H <- sum(obs==sim)
+
+   # M: Missed
+   obs.events.index <- which(obs==trgt)
+   M <- sum(sim[obs.events.index] != obs[obs.events.index])
+
+   # F: False alarms
+   sim.events.index <- which(sim==trgt)
+   F <- sum(sim[sim.events.index] != obs[sim.events.index])
+
+   # CN: correct negative
+   obs.noevents.index <- which(obs!=trgt)
+   sim.noevents.index <- which(sim!=trgt)
+   CN <- length( intersect(obs.noevents.index, sim.noevents.index) )
+
+   # He:
+   He <- (H+F)*(H+M)/Ne
+
+   # CSI
+   CSI <- H / (H+F+M)
+     
+   return(CSI)
+     
+} # 'CSI' end
+
+
+################################################################################
+##                       'CSI': Critical Success Index                        ## 
+##                        (also known as Threat Score)                        ## 
+##                   (also known as Ratio of Verification)                    ##
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                        ##
+################################################################################
+# Created: 09-Mar-2016                                                        ##
+# Updates:                                                                    ##
+################################################################################
+CSI.matrix <- function (sim, obs, trgt=NA, breaks=NA, na.rm=TRUE, verbose=TRUE, ...){ 
+
+  # Checking that 'sim' and 'obs' have the same dimensions
+  if ( all.equal(dim(sim), dim(obs)) != TRUE )
+    stop( paste("Invalid argument: dim(sim) != dim(obs) ( [", 
+          paste(dim(sim), collapse=" "), "] != [", 
+          paste(dim(obs), collapse=" "), "] )", sep="") )
+ 
+  CSI <- rep(NA, ncol(obs))       
+          
+  CSI <- sapply(1:ncol(obs), function(i,x,y) { 
+                 CSI[i] <- CSI.default( x[,i], y[,i], trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose, ... )
+               }, x=sim, y=obs )    
+                     
+  names(CSI) <- colnames(obs)
+  
+  return(CSI)
+     
+} # 'CSI.matrix' end
+
+
+################################################################################
+##                       'CSI': Critical Success Index                        ## 
+##                        (also known as Threat Score)                        ## 
+##                   (also known as Ratio of Verification)                    ## 
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                        ##
+################################################################################
+# Created: 09-Mar-2016                                                        ##
+# Updates:                                                                    ##
+################################################################################
+CSI.data.frame <- function (sim, obs, trgt, breaks, na.rm=TRUE, verbose=TRUE, ...){ 
+ 
+  sim <- as.matrix(sim)
+  obs <- as.matrix(obs)
+   
+  CSI.matrix(sim, obs, trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose,...)
+     
+} # 'CSI.data.frame' end
+
+
+
+################################################################################
+##                       'CSI': Critical Success Index                        ## 
+##                        (also known as Threat Score)                        ## 
+##                   (also known as Ratio of Verification)                    ## 
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                        ##
+################################################################################
+# Created: 09-Mar-2016                                                        ##
+# Updates:                                                                    ##
+################################################################################
+CSI.zoo <- function(sim, obs, trgt, breaks, na.rm=TRUE, verbose=TRUE, ...){
+    
+    sim <- zoo::coredata(sim)
+    if (is.zoo(obs)) obs <- zoo::coredata(obs)
+    
+    if (is.matrix(sim) | is.data.frame(sim)) {
+       CSI.matrix(sim, obs, na.rm=na.rm, ...)
+    } else NextMethod(sim, obs, trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose,...)
+     
+  } # 'CSI.zoo' end
+
