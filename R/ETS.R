ae93368499e9fd5a35e26fdf19120c6e51bee305
hzambran <mzb.devel@gmail.com>
2016-08-16 20:07:02 -0300

    now it shows intermediate values, including for for matrix/data.frame

--- R/ETS.R
+++ R/ETS.R
@@ -10,7 +10,7 @@
 # Author : Mauricio Zambrano-Bigiarini                                        ##
 ################################################################################
 # Created: 09-Mar-2016                                                        ##
-# Updates: 09-Aug-2016                                                        ##
+# Updates: 09-Aug-2016 ; 16-Aug-2016                                          ##
 ################################################################################
 # Reference: -) Jolliffe, I.T., Stephenson, D.B. (Eds.), 2003.                ##
 #               Forecast verification: A practitioners guide in atmospheric   ##
@@ -32,33 +32,39 @@
 
 ETS <-function(sim, obs, trgt=NA, ...) UseMethod("ETS")
 
-ETS.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, labels=NA, na.rm=TRUE, verbose=TRUE, ...){ 
+ETS.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=TRUE,
+                        labels=NA, na.rm=TRUE, out.type=c("single", "full"), 
+                        verbose=TRUE, ...){ 
+
+   # Checking that 'trgt' was provided 
+   if (is.na(trgt)) 
+     stop("Missing argument: You have to provide 'trgt' to compute PC !")    
+
+   # Getting the initial length of 'obs'
+   L.obs.ini <- length(obs)
 
    if ( is.na(match(class(sim), c("factor"))) | 
-        is.na(match(class(obs), c("factor"))) 
-      ) { if ( (missing(breaks)) ) {
-         stop("Missing argument: 'breaks' have to be provided when 'sim' & 'obs' are not factors !")  
-       } else {
-          if (verbose) message("Using 'breaks' to identify factor classes....")
-          if (!is.na(labels)) {
-            sim <- cut(sim, breaks=breaks, include.lowest=include.lowest, labels=labels)
-            obs <- cut(obs, breaks=breaks, include.lowest=include.lowest, labels=labels)
-          } else {
-              sim <- cut(sim, breaks=breaks, include.lowest=include.lowest)
-              obs <- cut(obs, breaks=breaks, include.lowest=include.lowest)
-            }
-         } 
-       }
+        is.na(match(class(obs), c("factor"))) ) { 
+      if ( ( (class(breaks)=="numeric") | (class(breaks)=="integer") ) ) {
+        if (verbose) message("[ Using 'breaks' to identify factor classes....]")
+        if (class(labels)=="character") {
+          sim <- cut(sim, breaks=breaks, right=!include.lowest, labels=labels)
+          obs <- cut(obs, breaks=breaks, right=!include.lowest, labels=labels)
+        } else {
+            sim <- cut(sim, breaks=breaks, right=!include.lowest)
+            obs <- cut(obs, breaks=breaks, right=!include.lowest)
+          } # ELSE end
+         if (verbose) message("[ classes(obs): ", paste(levels(obs), collapse = ", "), " ]")
+      } else stop("Missing argument: 'breaks' have to be provided when 'sim' & 'obs' are not factors !")  
+   } # IF end
 
+   # Checking that 'obs' and 'sim' has the same classes
    if (!(all.equal(levels(sim), levels(obs))))
      warning("'sim' and 'obs' do not have the same classes.")   
 
-   if (missing(trgt)) {
-     stop("Missing argument: You have to provide 'trgt' to compute ETS !")    
-   } else {
-     if ( !(trgt %in% levels(sim) ) | !(trgt %in% levels(obs) ) )
-       stop("Invalid argument: 'trgt' has to be present in both 'sim' and 'obs' !")  
-     } # ELSE end
+   # Checking that 'trgt' is present in both 'sim' and 'obs'
+   if ( !(trgt %in% levels(sim) ) | !(trgt %in% levels(obs) ) )
+     stop("Invalid argument: 'trgt' has to be present in both 'sim' and 'obs' !")  
 
    vi <- valindex(sim, obs)
      
@@ -93,13 +99,32 @@ ETS.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, lab
    sim.noevents.index <- which(sim!=trgt)
    CN <- length( intersect(obs.noevents.index, sim.noevents.index) )
 
-   # He:
+   # He: Number of hits expected independent of observations (pure chance)
    He <- ( H + FA ) * ( H + M ) / Ne
 
    # ETS
    ETS <- ( H - He ) / ( H + FA + M - He)
      
-   return(ETS)
+   # If verbose, it shows some intermediate elements of the computation
+   if (verbose) message("Hits             : ", H)
+   if (verbose) message("False Alarms     : ", FA)
+   if (verbose) message("Missed           : ", M)
+   if (verbose) message("Correct Negatives: ", CN)
+   if (verbose) message("Hits by chance   : ", He)
+   if (verbose) message("Number of points : ", Ne)
+   if (L.obs.ini!=Ne) {
+     if (verbose) message("Discarded points : ", L.obs.ini-Ne)
+   } # IF end
+
+   # final output
+   if (out.type=="single") {
+        out <- ETS
+   } else {
+      out <- list(ETS.value=ETS, ETS.elements=c(H, FA, M, CN, He, Ne, L.obs.ini-Ne))
+      names(out[[2]]) <- c("Hits", "False Alarms", "Missed", "Correct Negatives", "Hits by chance", "Number of points", "Discarded points")
+     } # ELSE end 
+     
+   return(out)
      
 } # 'ETS' end
 
@@ -111,26 +136,49 @@ ETS.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, lab
 # Author : Mauricio Zambrano-Bigiarini                                        ##
 ################################################################################
 # Created: 09-Mar-2016                                                        ##
-# Updates:                                                                    ##
+# Updates: 16-Aug-2016                                                        ##
 ################################################################################
-ETS.matrix <- function (sim, obs, trgt=NA, breaks=NA, na.rm=TRUE, verbose=TRUE, ...){ 
+ETS.matrix <- function(sim, obs, trgt=NA, breaks=NA, include.lowest=TRUE,
+                       labels=NA, na.rm=TRUE, out.type=c("single", "full"), 
+                       verbose=FALSE, ...){ 
 
   # Checking that 'sim' and 'obs' have the same dimensions
   if ( all.equal(dim(sim), dim(obs)) != TRUE )
     stop( paste("Invalid argument: dim(sim) != dim(obs) ( [", 
           paste(dim(sim), collapse=" "), "] != [", 
           paste(dim(obs), collapse=" "), "] )", sep="") )
- 
-  ETS <- rep(NA, ncol(obs))       
+
+
+  out.type <- match.arg(out.type) 
+
+  ETS                <- rep(NA, ncol(obs))       
+  elements           <- matrix(NA, nrow=7, ncol=ncol(obs))
+  rownames(elements) <- c("Hits", "False Alarms", "Missed", "Correct Negatives", "Hits by chance", "Number of points", "Discarded points")
+  colnames(elements) <- colnames(obs)
           
-  ETS <- sapply(1:ncol(obs), function(i,x,y) { 
-                 ETS[i] <- ETS.default( x[,i], y[,i], trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose, ... )
-               }, x=sim, y=obs )    
-                     
-  names(ETS) <- colnames(obs)
+  if (out.type=="single") {
+    out <- sapply(1:ncol(obs), function(i,x,y) { 
+                   ETS[i] <- ETS.default( x[,i], y[,i], trgt=trgt, breaks=breaks, 
+                                      include.lowest=include.lowest, labels=labels, 
+                                      na.rm=na.rm, out.type=out.type, 
+                                      verbose=FALSE, ... )
+                 }, x=sim, y=obs )  
+    names(out) <- colnames(obs) 
+  } else { out <- lapply(1:ncol(obs), function(i,x,y) { 
+                         ETS.default( x[,i], y[,i], trgt=trgt, breaks=breaks, 
+                                      include.lowest=include.lowest, labels=labels, 
+                                      na.rm=na.rm, out.type=out.type, 
+                                      verbose=FALSE, ... )
+                       }, x=sim, y=obs ) 
+            for (i in 1:length(out) ) {
+               ETS[i] <- out[[i]][[1]]
+               elements[,i] <- as.numeric(out[[i]][[2]])
+            } # FOR end 
+            out <- list(ETS.value=ETS, ETS.elements=elements)
+          } # ELSE end                     
   
-  return(ETS)
-     
+  return(out)
+   
 } # 'ETS.matrix' end
 
 
@@ -141,15 +189,20 @@ ETS.matrix <- function (sim, obs, trgt=NA, breaks=NA, na.rm=TRUE, verbose=TRUE, 
 # Author : Mauricio Zambrano-Bigiarini                                        ##
 ################################################################################
 # Created: 09-Mar-2016                                                        ##
-# Updates:                                                                    ##
+# Updates: 16-Aug-2016                                                        ##
 ################################################################################
-ETS.data.frame <- function (sim, obs, trgt, breaks, na.rm=TRUE, verbose=TRUE, ...){ 
- 
+ETS.data.frame <- function(sim, obs, trgt=NA, breaks=NA, include.lowest=TRUE,
+                           labels=NA, na.rm=TRUE, out.type=c("single", "full"), 
+                           verbose=FALSE, ...){ 
+
   sim <- as.matrix(sim)
   obs <- as.matrix(obs)
+
+  out.type <- match.arg(out.type) 
    
-  ETS.matrix(sim, obs, trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose,...)
-     
+  ETS.matrix(sim, obs, trgt=trgt, breaks=breaks, include.lowest=include.lowest, 
+            labels=labels, na.rm=na.rm, out.type=out.type, verbose=FALSE, ... )
+      
 } # 'ETS.data.frame' end
 
 
@@ -161,16 +214,20 @@ ETS.data.frame <- function (sim, obs, trgt, breaks, na.rm=TRUE, verbose=TRUE, ..
 # Author : Mauricio Zambrano-Bigiarini                                        ##
 ################################################################################
 # Created: 09-Mar-2016                                                        ##
-# Updates:                                                                    ##
+# Updates: 16-Aug-2016                                                        ##
 ################################################################################
-ETS.zoo <- function(sim, obs, trgt, breaks, na.rm=TRUE, verbose=TRUE, ...){
-    
+ETS.zoo <- function(sim, obs, trgt=NA, breaks=NA, include.lowest=TRUE,
+                        labels=NA, na.rm=TRUE, out.type=c("single", "full"), 
+                        verbose=TRUE, ...){ 
+  
     sim <- zoo::coredata(sim)
     if (is.zoo(obs)) obs <- zoo::coredata(obs)
     
     if (is.matrix(sim) | is.data.frame(sim)) {
-       ETS.matrix(sim, obs, na.rm=na.rm, ...)
-    } else NextMethod(sim, obs, trgt=trgt, breaks=breaks, na.rm=na.rm, verbose=verbose,...)
-     
+       ETS.matrix(sim, obs, trgt=trgt, breaks=breaks, include.lowest=include.lowest, 
+                 labels=labels, na.rm=na.rm, out.type=out.type, verbose=verbose, ... )
+    } else NextMethod(sim, obs, trgt=trgt, breaks=breaks, include.lowest=include.lowest, 
+                      labels=labels, na.rm=na.rm, out.type=out.type, verbose=verbose, ... )
+   
   } # 'ETS.zoo' end
 
