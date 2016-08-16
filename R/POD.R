275c23a5e458a92debaef18abb22ccb63b2aa45d
hzambran <mzb.devel@gmail.com>
2016-08-16 19:25:12 -0300

    corrected a flag in the computation of the intermediate values

--- R/POD.R
+++ R/POD.R
@@ -10,7 +10,7 @@
 # Author : Mauricio Zambrano-Bigiarini                                        ##
 ################################################################################
 # Created: 06-Mar-2016                                                        ##
-# Updates: 08-Mar-2016 ; 09-Mar-2016                                          ##
+# Updates: 08-Mar-2016 ; 09-Mar-2016 ; 09-Aug-2016                            ##
 ################################################################################
 # Reference: -) Jolliffe, I.T., Stephenson, D.B. (Eds.), 2003.                ##
 #               Forecast verification: A practitioners guide in atmospheric   ##
@@ -69,15 +69,24 @@ POD.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, lab
    Ne <- length(obs)
      
    # H: Hits
-   H <- sum(obs==sim)
+   #H <- sum(obs==sim)
+   obs.events.index <- which(obs==trgt)
+   sim.events.index <- which(sim==trgt)
+   H <- length( intersect(obs.events.index, sim.events.index) )
 
    # M: Missed
-   obs.events.index <- which(obs==trgt)
-   M <- sum(sim[obs.events.index] != obs[obs.events.index])
+   #obs.events.index <- which(obs==trgt)
+   #M <- sum(sim[obs.events.index] != obs[obs.events.index])
+   obs.events.index   <- which(obs==trgt)
+   sim.noevents.index <- which(sim!=trgt)
+   M <- length( intersect(obs.events.index, sim.noevents.index) )
 
    # F: False alarms
-   sim.events.index <- which(sim==trgt)
-   F <- sum(sim[sim.events.index] != obs[sim.events.index])
+   #sim.events.index <- which(sim==trgt)
+   #FA <- sum(sim[sim.events.index] != obs[sim.events.index])
+   obs.noevents.index <- which(obs!=trgt)
+   sim.events.index   <- which(sim==trgt)
+   FA <- length( intersect(obs.noevents.index, sim.events.index) )
 
    # CN: correct negative
    obs.noevents.index <- which(obs!=trgt)
@@ -85,7 +94,7 @@ POD.default <- function (sim, obs, trgt=NA, breaks=NA, include.lowest=FALSE, lab
    CN <- length( intersect(obs.noevents.index, sim.noevents.index) )
 
    # He:
-   He <- (H+F)*(H+M)/Ne
+   He <- ( H + FA ) * ( H + M ) / Ne
 
    # POD
    POD <- H / (H+M)
