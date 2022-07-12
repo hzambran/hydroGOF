4ac7624c5238b5e6540e494f7e552d8d086ecc68
Mauricio Zambrano-Bigiarini <mzb.devel@gmail.com>
2022-07-12 11:26:46 -0400

    added KGElf for several object classes

--- R/KGElf.R
+++ R/KGElf.R
@@ -10,7 +10,7 @@
 # Author : Mauricio Zambrano-Bigiarini                                         #
 ################################################################################
 # Started: 2017                                                                #
-# Updates: 07-Jul-2022 ; 11-Jul-2022                                           #
+# Updates: 07-Jul-2022 ; 11-Jul-2022 ;12-Jul-2022                              #
 ################################################################################
 # The optimal value of KGElf is 1
 
@@ -28,20 +28,127 @@ KGElf <- function(sim, obs, ...) UseMethod("KGElf")
 
 # epsilon: By default it is set at one hundredth of the mean flow. See Pushpalatha et al. (2012)
 KGElf.default <- function(sim, obs, s=c(1,1,1), na.rm=TRUE, 
-                          method=c("2009", "2012"), out.type=c("single", "full"), 
-                          epsilon=1/100, ...) {
+                          method=c("2009", "2012"), 
+                          FUN=function(x) 1/x,
+                          ...,
+                          epsilon=c("Pushpalatha2012", "otherFactor", "otherValue"), 
+                          epsilon.value=NA) { 
   
+  # Checking 'method' and 'epsilon''
+  method   <- match.arg(method)
+  epsilon <- match.arg(epsilon)
+
   # 1) KGE(Q): KGE (2009 or 2012)
-  kge <- KGE(sim=sim, obs=obs, method=method)
+  kge <- KGE(sim=sim, obs=obs, s=s, na.rm=na.rm, method=method, out.type="single")
   
   # 2) KGE(1/Q): KGE based on Garcia et al. (2017), with epsilon based on Pushpalatha et al. (2012)
-  sim.lf <- 1 / ( sim + epsilon*mean(obs, na.rm = TRUE) )
-  obs.lf <- 1 / ( obs + epsilon*mean(obs, na.rm = TRUE) )
-  kge.lf <- KGE(sim=sim.lf, obs=obs.lf, method=method)
+  new <- preproc(sim=sim, obs=obs, FUN=FUN, ...,
+                 epsilon=epsilon, epsilon.value=epsilon.value)
+  sim.lf <- new[["sim"]]
+  obs.lf <- new[["obs"]]
+  kge.lf <- KGE(sim=sim.lf, obs=obs.lf, s=s, na.rm=na.rm, method=method, out.type="single")
   
-  # 3) [KGE(Q) + KGE(1/Q)] / 2 : ggregated goodness-of-fit value
+  # 3) [KGE(Q) + KGE(1/Q)] / 2 : comprehensive goodness-of-fit value
   out <- (kge + kge.lf) / 2
     
   return(out)
   
-}
+} # 'KGElf.default' END
+
+
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                         #
+################################################################################
+# Started: 12-Jul-2022                                                         #
+# Updates:                                                                     #
+################################################################################
+KGElf.matrix <- function(sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                         method=c("2009", "2012"), 
+                         FUN=function(x) 1/x,
+                         ...,
+                         epsilon=c("Pushpalatha2012", "otherFactor", "otherValue"), 
+                         epsilon.value=NA) { 
+
+  # Checking that 'sim' and 'obs' have the same dimensions
+  if ( all.equal(dim(sim), dim(obs)) != TRUE )
+     stop( paste("Invalid argument: dim(sim) != dim(obs) ( [", 
+           paste(dim(sim), collapse=" "), "] != [", 
+           paste(dim(obs), collapse=" "), "] )", sep="") )
+           
+  # If the user provided a value for 's'
+  if (!all.equal(s, c(1,1,1)) )  {
+     if ( length(s) != 3 ) stop("Invalid argument: lenght(s) must be equal to 3 !")
+     if ( sum(s) != 1 )    stop("Invalid argument: sum(s) must be equal to 1.0 !")
+  } # IF end
+           
+  method   <- match.arg(method)
+  epsilon <- match.arg(epsilon)
+
+  ifelse(method=="2012", vr.stg <- "Gamma", vr.stg <- "Alpha")
+
+  KGElf              <- rep(NA, ncol(obs))       
+  elements           <- matrix(NA, nrow=3, ncol=ncol(obs))
+  rownames(elements) <- c("r", "Beta", vr.stg)
+  colnames(elements) <- colnames(obs)
+          
+  out <- sapply(1:ncol(obs), function(i,x,y) { 
+                   KGElf[i] <- KGElf.default( x[,i], y[,i], s=s, na.rm=na.rm, 
+                                              method=method, FUN=FUN, ..., 
+                                              epsilon=epsilon, 
+                                              epsilon.value=epsilon.value )
+                 }, x=sim, y=obs )  
+  names(out) <- colnames(obs)                     
+  
+  return(out)
+     
+} # 'KGElf.matrix' end
+
+
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                         #
+################################################################################
+# Started: 12-Jul-2022                                                         #
+# Updates:                                                                     #
+################################################################################
+KGElf.data.frame <- function(sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                             method=c("2009", "2012"), 
+                             FUN=function(x) 1/x,
+                             ...,
+                             epsilon=c("Pushpalatha2012", "otherFactor", "otherValue"), 
+                             epsilon.value=NA) { 
+ 
+  sim <- as.matrix(sim)
+  obs <- as.matrix(obs)
+  
+  method   <- match.arg(method)
+  epsilon <- match.arg(epsilon) 
+   
+  KGElf.matrix(sim, obs, s=s, na.rm=na.rm, method=method, FUN=FUN, ..., 
+               epsilon=epsilon, epsilon.value=epsilon.value)
+     
+} # 'KGElf.data.frame' end
+
+
+################################################################################
+# Author : Mauricio Zambrano-Bigiarini                                         #
+################################################################################
+# Started: 12-Jul-2022                                                         #
+# Updates:                                                                     #
+################################################################################
+KGElf.zoo <- function(sim, obs, s=c(1,1,1), na.rm=TRUE, 
+                      method=c("2009", "2012"), 
+                      FUN=function(x) 1/x,
+                      ...,
+                      epsilon=c("Pushpalatha2012", "otherFactor", "otherValue"), 
+                      epsilon.value=NA) { 
+    
+    sim <- zoo::coredata(sim)
+    if (is.zoo(obs)) obs <- zoo::coredata(obs)
+    
+    if (is.matrix(sim) | is.data.frame(sim)) {
+       KGElf.matrix(sim, obs, s=s, na.rm=na.rm, method=method, FUN=FUN, ..., 
+                    epsilon=epsilon, epsilon.value=epsilon.value)
+    } else NextMethod(sim, obs, s=s, na.rm=na.rm, method=method, FUN=FUN, ..., 
+                      epsilon=epsilon, epsilon.value=epsilon.value)
+     
+  } # 'KGElf.zoo' end
