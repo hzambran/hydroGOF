e084cc824f8d4b699110465d00e8cc15ce0e5ece
Mauricio Zambrano-Bigiarini <mzb.devel@gmail.com>
2024-01-19 13:08:58 -0300

    now it makes treatment of missing mavlues in 'sim' and 'obs'

--- R/R2.R
+++ R/R2.R
@@ -10,6 +10,25 @@
 .R2 <- function(sim, obs, fun=NULL, ...,
                 epsilon.type=c("none", "Pushpalatha2012", "otherFactor", "otherValue"), 
                 epsilon.value=NA) {
+
+  # index of those elements that are present both in 'sim' and 'obs' (NON- NA values)
+  vi <- valindex(sim, obs)
+   
+  if (length(vi) > 0) {	 
+    # Filtering 'obs' and 'sim', selecting only those pairs of elements 
+    # that are present both in 'x' and 'y' (NON- NA values)
+    obs <- obs[vi]
+    sim <- sim[vi]
+
+    if (!is.null(fun)) {
+      fun1 <- match.fun(fun)
+      new  <- preproc(sim=sim, obs=obs, fun=fun1, ..., 
+                      epsilon.type=epsilon.type, epsilon.value=epsilon.value)
+      sim  <- new[["sim"]]
+      obs  <- new[["obs"]]
+    } # IF end 
+  } # IF end    
+
   Om    <- mean(obs)
   SSres <- sum( (obs - sim)^2 )
   SStot <- sum( (obs - Om)^2 )
